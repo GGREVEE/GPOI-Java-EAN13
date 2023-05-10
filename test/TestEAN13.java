171bc8c2d4d304d603a0e87414c0eb47af252bd8
GGREVEE <andreagulfo25@gmail.com>
2023-05-10 10:27:21 +0200

    Commit n1

---
+++ test/TestEAN13.java
@@ -0,0 +1,41 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+import org.junit.Before;
+
+
+public class TestEAN13 {
+    private static EAN13 ean;
+
+    @Before
+    public void init(){
+    ean = new EAN13();
+
+    }  
+    @Test
+    public void testEanVerificaCodiceValido(){
+        ean.setCodice("3614273613422");
+        assertTrue(ean.verificaCodice());
+        
+    }
+    @Test
+    public void testEanVerificaCodiceVuoto(){
+        ean.setCodice("");
+        assertThrows(NullPointerException.class,()-> ean.verificaCodice());
+    }
+    @Test
+    public void  testEanVerificaCodiceNonValido(){
+        ean.setCodice("3614273613427");
+        assertFalse(ean.verificaCodice());
+    }
+    @Test
+    public void testEanVerificaCodiceLunghezzaNonValida(){
+        ean.setCodice("3614273613427345667");
+        assertFalse(ean.verificaCodice());
+    }
+
+}
